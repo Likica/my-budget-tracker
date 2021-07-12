4eacaad61401ec2ff9183419c6da44c09d46f329
Coco Puff <fahrija_wyroski@live.com>
2021-07-11 20:54:22 -0500

    update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const compression = require("compression");
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(logger("dev"));
